ace6cd93b08db0af3d5e022250d6057d2606540d
Ojaswy Akella <32218681+Ojaswy@users.noreply.github.com>
2019-03-23 19:56:28 +0530

    Update awake.py

--- Awake/awake.py
+++ Awake/awake.py
@@ -20,7 +20,7 @@ print ('\nTASK I:\n')
 cern_time=pytz.timezone('Europe/Zurich')
 unix_time=float(file[:18])/100000000
 utc=datetime.utcfromtimestamp(unix_time)
-print("Time UTC is",utc)
+print("Time in UTC is",utc)
 cern=pytz.utc.localize(utc).astimezone(cern_time)
 print("Time in Switzerland/CERN is",cern)
 
