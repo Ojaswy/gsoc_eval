8ee83651a1749643a5a1fd279a54263a504a253c
Ojaswy Akella <32218681+Ojaswy@users.noreply.github.com>
2019-03-23 19:59:13 +0530

    Update time.py

--- Awake/other_files/time.py
+++ Awake/other_files/time.py
@@ -5,6 +5,6 @@ file = '1541962108935000000_167_838.h5'
 cern_time=pytz.timezone('Europe/Zurich')
 unix_time=float(file[:18])/100000000
 utc=datetime.utcfromtimestamp(unix_time)
-print("Time UTC is",utc)
+print("Time in UTC is",utc)
 cern=pytz.utc.localize(utc).astimezone(cern_time)
 print("Time in Switzerland/CERN is",cern)
